e435b0ef279e0e6e9d271185e26342c33f144860
Marcio Coelho <marcioc@gmail.com>
2019-06-18 16:27:26 -0300

    gitignore teste 01

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.get('/', (req, res, next) => {
-  console.log('Api funcionando...');
+  console.log('Api funcionando...', process.env.NODE_ENV);
 })
 
 // app.get('/users', verifyJWT, (req, res, next) => {
